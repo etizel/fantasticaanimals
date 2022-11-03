5fde8c13973e3815fb3cd2240bfe322867806b82
etiiizel <franciscoetizel@gmail.com>
2022-11-03 16:28:21 -0300

    animation scroll II

--- app.js
+++ app.js
@@ -95,23 +95,30 @@ activeLinks();
 
 
 
+
+
+function initAnimation() {
 const sections = document.querySelectorAll('.js-scroll');
+
+
+const windowMetade = window.innerHeight * 0.6;
 
 function animaScroll() {
   sections.forEach((section) => {
-    const sectionTop = section.getBoundingClientRect().top - 500;
-    if(sectionTop < 0) {
+    const sectionTop = section.getBoundingClientRect().top;
+    const isSectionVisible = (sectionTop - windowMetade) < 0;
+    if(isSectionVisible) {
       section.classList.add('active');
     }
   })
 }
-
-
 window.addEventListener('scroll', animaScroll)
 
+}
 
 
 
+initAnimation();
 
 
 
