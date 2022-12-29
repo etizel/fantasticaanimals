704e81234e0750562b0c07100928cf8112e40e2c
etiiizel <franciscoetizel@gmail.com>
2022-12-29 18:03:51 -0300

    modal.js

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -5,11 +5,18 @@ const botaoFechar = document.querySelector('[data-modal = "fechar"]');
 const containerModal = document.querySelector('[data-modal = "container"]');
 
 function abrirModal(event) {
-  event.preventDefault;
-  console.log(event);
+  event.preventDefault();
+  containerModal.classList.add('active')
 }
 
-function fecharModal() {}
+function fecharModal() {
+  containerModal.classList.remove('active');
+}
+
+function clickFora(event) {
+  fecharModal();
+}
 
 botaoAbrir.addEventListener('click', abrirModal);
 botaoFechar.addEventListener('click', fecharModal);
+containerModal.addEventListener('click', clickFora)
