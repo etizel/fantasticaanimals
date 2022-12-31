d512942108d8337054ae863b00cc89fe7fa60a9f
etiiizel <franciscoetizel@gmail.com>
2022-12-31 12:13:55 -0300

    tooltip

--- js/modules/inittool.js
+++ js/modules/inittool.js
@@ -2,10 +2,19 @@ export default function initToolTip() {}
 
 const toolTip = document.querySelectorAll('[data-tooltip]');
 
+toolTip.forEach((item) => {
+  item.addEventListener('mouseover', activeMouse);
+});
+
 function activeMouse(event) {
-  console.log(event);
+  const tooltipBox = criarToolTipBox(this);
 }
 
-toolTip.forEach((item) => {
-  item.addEventListener('click', activeMouse);
-});
+function criarToolTipBox(element) {
+  const toolTipBox = document.createElement('div');
+  const text = element.getAttribute('aria-label');
+  toolTipBox.classList.add('tooltip');
+  toolTipBox.innerText = text;
+  document.body.appendChild(toolTipBox);
+  return toolTipBox;
+}
