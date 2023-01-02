b970e89391e792a054c26c637abe25d9ee4f0dc4
etiiizel <franciscoetizel@gmail.com>
2023-01-02 15:10:20 -0300

    tooltip

--- js/modules/inittool.js
+++ js/modules/inittool.js
@@ -2,12 +2,27 @@ export default function initToolTip() {}
 
 const tooltips = document.querySelectorAll('[data-tooltip]');
 
-function onMouseOver(event) {
-  const div = document.createElement('div');
-  div.innerText = 'Próximo ao estacionamento';
-  const active = event;
-}
-
 tooltips.forEach((item) => {
   item.addEventListener('mouseover', onMouseOver);
 });
+
+function onMouseOver(event) {
+  const toolTipBox = criarToolTipBox(this);
+  toolTipBox.style.top = event.pageY + 'px';
+  toolTipBox.style.left = event.pageX + 'px';
+
+  this.addEventListener('mouseleave', onMouseLeave);
+
+  function onMouseLeave() {
+    toolTipBox.remove();
+  }
+}
+
+function criarToolTipBox(element) {
+  const toolTipBox = document.createElement('div');
+  toolTipBox.classList.add('tooltip');
+  const text = element.getAttribute('aria-label');
+  toolTipBox.innerText = text;
+  document.body.appendChild(toolTipBox);
+  return toolTipBox;
+}
