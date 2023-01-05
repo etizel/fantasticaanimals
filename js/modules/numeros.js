befc71270592db1a6bb33ecc8858ecafe48be3b5
etiiizel <franciscoetizel@gmail.com>
2023-01-05 16:23:22 -0300

    anima number

--- js/modules/numeros.js
+++ js/modules/numeros.js
@@ -1,14 +1,30 @@
-export default function initNumber() {}
+export default function initNumber() {
+  function animaNumeros() {
+    const numeros = document.querySelectorAll('[data-numero]');
 
-const numeros = document.querySelectorAll('[data-numero]');
+    numeros.forEach((numero) => {
+      const total = +numero.innerText;
+      const incremento = Math.floor(total / 100);
+      let start = 0;
+      const timer = setInterval(() => {
+        start = start + incremento;
+        numero.innerText = start;
+        if (start > total) {
+          numero.innerText = total;
+          clearInterval(timer);
+        }
+      }, 25 * Math.random());
+    });
+  }
 
-numeros.forEach((numero) => {
-  const total = +numero.innerText;
-  const incremento = Math.floor(total / 100);
-  let start = 0;
-  const timer = setInterval(() => {
-    start = start + incremento;
-    numero.innerText = start;
-    if (start > total) clearInterval(timer);
-  }, 0);
-});
+  function handleMutation(mutation) {
+    if (mutation[0].target.classList.contains('ativo'));
+    observer.disconnect();
+    animaNumeros();
+  }
+
+  const observeTarget = document.querySelector('.numeros');
+  const observer = new MutationObserver(handleMutation);
+
+  observer.observe(observeTarget, { attributes: true });
+}
