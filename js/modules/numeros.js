a63a602ff20c4d7b19d90dcf98f80b7f10a076bf
etiiizel <franciscoetizel@gmail.com>
2023-01-05 14:07:54 -0300

    anima number

--- js/modules/numeros.js
+++ js/modules/numeros.js
@@ -1,1 +1,14 @@
 export default function initNumber() {}
+
+const numeros = document.querySelectorAll('[data-numero]');
+
+numeros.forEach((numero) => {
+  const total = +numero.innerText;
+  const incremento = Math.floor(total / 100);
+  let start = 0;
+  const timer = setInterval(() => {
+    start = start + incremento;
+    numero.innerText = start;
+    if (start > total) clearInterval(timer);
+  }, 0);
+});
