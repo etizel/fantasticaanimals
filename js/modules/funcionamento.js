31849684913b32e530f352fb0fd660ae451712a3
etiiizel <franciscoetizel@gmail.com>
2023-01-06 13:52:57 -0300

    Date

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,16 +1,20 @@
-export default function initFuncionamento() {}
+export default function initFuncionamento() {
+  const funcionamento = document.querySelector('[data-semana]');
 
-const funcionamento = document.querySelector('[data-semana]');
+  const diasSemana = funcionamento.dataset.semana.split(',').map(Number);
 
-const diasSemana = funcionamento.dataset.semana.split(',').map(Number);
+  const horarioSemana = funcionamento.dataset.horario.split(',').map(Number);
 
-const horarioSemana = funcionamento.dataset.horario.split(',').map(Number);
+  const dataAgora = new Date();
+  const diaAgora = dataAgora.getDay();
+  const horarioAgora = dataAgora.getHours;
 
-const dataAgora = new Date();
-const diaAgora = dataAgora.getDay();
-const horarioAgora = dataAgora.getHours;
+  const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
 
-const teste = [1, 2, 3, 4, 5].indexOf(6);
-const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
+  const horarioAberto =
+    horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1];
 
-console.log(horarioAgora);
+  if (semanaAberto && horarioAberto) {
+    funcionamento.classList.add('open');
+  }
+}
