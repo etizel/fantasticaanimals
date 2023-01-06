af8cf11ac1877e5a3c13dd495619ced1ac2989b2
etiiizel <franciscoetizel@gmail.com>
2023-01-06 13:30:58 -0300

    Date

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,6 +1,16 @@
 export default function initFuncionamento() {}
 
 const funcionamento = document.querySelector('[data-semana]');
+
 const diasSemana = funcionamento.dataset.semana.split(',').map(Number);
 
-console.log(diasSemana);
+const horarioSemana = funcionamento.dataset.horario.split(',').map(Number);
+
+const dataAgora = new Date();
+const diaAgora = dataAgora.getDay();
+const horarioAgora = dataAgora.getHours;
+
+const teste = [1, 2, 3, 4, 5].indexOf(6);
+const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
+
+console.log(horarioAgora);
