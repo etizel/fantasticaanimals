0ae3c1f2c21f5dff27bda6f8215aa59d2eed7afb
etiiizel <franciscoetizel@gmail.com>
2023-04-17 19:21:48 -0300

    refatorar-accordion-activeClass

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -2,30 +2,24 @@ export default class Accordion {
   constructor(accordionList, activeClass) {
     this.accordionList = document.querySelectorAll(accordionList);
     this.activeClass = activeClass;
- 
   }
 
-
- toggleAccordion(item) {
-    item.classList.toggle(activeClass);
-    item.nextElementSibling.classList.toggle(activeClass);
+  toggleAccordion(item) {
+    item.classList.toggle(this.activeClass);
+    item.nextElementSibling.classList.toggle(this.activeClass);
   }
 
   activeAccordionEvent() {
     this.accordionList.forEach((item) => {
-      item.addEventListener('click',() =>  this.toggleAccordion(item));
+      item.addEventListener('click', () => this.toggleAccordion(item));
     });
   }
- 
-    accordionList[0].classList.add(activeClass);
-    accordionList[0].nextElementSibling.classList.add(activeClass);
 
-    init() {
-      if (this.accordionList.length) {
-        this.activeAccordionEvent();
+  init() {
+    if (this.accordionList.length) {
+      this.toggleAccordion(this.accordionList[0]);
+      this.activeAccordionEvent();
     }
     return this;
   }
-  }
-
-
+}
