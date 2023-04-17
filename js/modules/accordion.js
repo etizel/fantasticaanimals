20ef895027fa7842d1f9922da3cf1a2fbcd4ad17
etiiizel <franciscoetizel@gmail.com>
2023-04-17 18:55:18 -0300

    refatorar-accordion-methods

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,18 +1,28 @@
-export default function initAccordion() {
-  const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
-  const activeClass = 'ativo';
+export default class Accordion {
+  constructor(accordionList, activeClass) {
+    this.accordionList = document.querySelectorAll(accordionList);
+    this.activeClass = activeClass;
+  }
 
-  function activeAccordion() {
+
+ activeAccordion() {
     this.classList.toggle(activeClass);
     this.nextElementSibling.classList.toggle(activeClass);
   }
 
+  activeAccordionEvent() {
+    this.accordionList.forEach((item) => {
+      item.addEventListener('click', this.activeAccordion);
+    });
+  }
   if (accordionList.length) {
     accordionList[0].classList.add(activeClass);
     accordionList[0].nextElementSibling.classList.add(activeClass);
 
-    accordionList.forEach((item) => {
-      item.addEventListener('click', activeAccordion);
-    });
+  
+  }
+
+  init() {
+
   }
 }
