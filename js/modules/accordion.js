243de34a7bb7c4e472e27c005efb957c56df7e4b
etiiizel <franciscoetizel@gmail.com>
2023-04-17 19:26:03 -0300

    refatorar-accordion-comments

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -4,18 +4,22 @@ export default class Accordion {
     this.activeClass = activeClass;
   }
 
+  // add class active in accordion
   toggleAccordion(item) {
     item.classList.toggle(this.activeClass);
     item.nextElementSibling.classList.toggle(this.activeClass);
   }
 
+  // add event in accordion
   activeAccordionEvent() {
     this.accordionList.forEach((item) => {
       item.addEventListener('click', () => this.toggleAccordion(item));
     });
   }
 
+  // start function
   init() {
+    // active first itemn
     if (this.accordionList.length) {
       this.toggleAccordion(this.accordionList[0]);
       this.activeAccordionEvent();
