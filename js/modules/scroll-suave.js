e1fd7128c98dac2ec5217e59774121dc3d01a259
etiiizel <franciscoetizel@gmail.com>
2023-04-17 17:31:13 -0300

    bind(this)

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,11 +1,12 @@
 export default class ScrollSuave {
   constructor(links, options) {
-    this.linksInternos = document.querySelector(links);
+    this.linksInternos = document.querySelectorAll(links);
     if (options === undefined) {
       this.options = { behavior: 'smooth', block: 'start' };
     } else {
       this.options = options;
     }
+    this.scrollToSection = this.scrollToSection.bind(this);
   }
 
   scrollToSection(event) {
@@ -17,7 +18,7 @@ export default class ScrollSuave {
 
   addEventLink() {
     this.linksInternos.forEach((link) => {
-      link.addEventListener('click', this.scrollToSection.bind(this));
+      link.addEventListener('click', this.scrollToSection);
     });
   }
 
