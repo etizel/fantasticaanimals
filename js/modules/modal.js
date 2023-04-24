e5a82df23d6f765c9ca677206f8c01c264d86f27
etiiizel <franciscoetizel@gmail.com>
2023-04-24 12:46:59 -0300

    comments

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -10,21 +10,25 @@ export default class Modal {
     this.cliqueForaModal = this.cliqueForaModal.bind(this);
   }
 
+  // abre ou fecha o modal
   toggleModal() {
     this.containerModal.classList.toggle(this.activeClass);
   }
 
+  // adiciona o evento de toggle ao modal
   eventModal(event) {
     event.preventDefault();
     this.toggleModal();
   }
 
+  // fecha o modal ao clicar do lado de fora
   cliqueForaModal(event) {
     if (event.target === this) {
       this.toggleModal(event);
     }
   }
 
+  // adiciona os eventos ao modal
   addEventModal() {
     this.botaoAbrir.addEventListener('click', this.eventModal);
     this.botaoFechar.addEventListener('click', this.eventModal);
