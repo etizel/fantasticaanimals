4f10aa58e1fd3b1051b241f8ae464920802f980f
etiiizel <franciscoetizel@gmail.com>
2023-04-27 17:09:22 -0300

    fetch-bitcoin-script

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetchanimais.js';
-import initFetchBitcoin from './modules/fetch-bitcoin.js';
+import fetchBitcoin from './modules/fetch-bitcoin.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -38,6 +38,6 @@ tooltip.init();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
-initFetchBitcoin();
 initAnimacaoScroll();
 fetchAnimais('../../animaisapi.json', '.numeros-grid');
+fetchBitcoin();
