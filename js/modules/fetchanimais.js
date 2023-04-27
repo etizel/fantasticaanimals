5a03dd6e2efd3b5be9e71785698a986d196dd51e
etiiizel <franciscoetizel@gmail.com>
2023-04-27 12:04:20 -0300

    refatoração-fetch-animaisx

---
+++ js/modules/fetchanimais.js
@@ -0,0 +1,8 @@
+import AnimaNumeros from './anima-numeros.js';
+
+export default function initFetchAnimais() {
+  function createAnimal(animal) {
+    const div = document.createElement('div');
+    div.classList.add('numero-animal');
+  }
+}
