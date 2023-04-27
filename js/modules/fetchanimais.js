e58fe32bd66a8b567efc5207e77c7f47af873bb5
etiiizel <franciscoetizel@gmail.com>
2023-04-27 12:22:25 -0300

    refatoração-fetch

--- js/modules/fetchanimais.js
+++ js/modules/fetchanimais.js
@@ -4,5 +4,28 @@ export default function initFetchAnimais() {
   function createAnimal(animal) {
     const div = document.createElement('div');
     div.classList.add('numero-animal');
+    div.innerHTML = `<h3>${animal.specie}</h3><span>${animal.total}</span>`;
+    return div;
   }
+
+  async function fetchAnimais(url) {
+    try {
+      const animaisResponse = await fetch(url);
+      const animaisJson = await animaisResponse.json();
+      const numerosGrid = document.querySelector('.numeros-grid');
+      animaisJson.forEach((animal) => {
+        const divAnimal = createAnimal(animal);
+        numerosGrid.appendChild(divAnimal);
+      });
+      const animaNumeros = new AnimaNumeros(
+        '[data-nuero]',
+        '.numeros',
+        'ativo',
+      );
+      animaNumeros.init();
+    } catch (erro) {
+      console.log(erro);
+    }
+  }
+  fetchAnimais('./animaisapi.json');
 }
